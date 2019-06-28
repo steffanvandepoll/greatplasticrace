f8ec440ade77d115ae11743eda3078c842640929
Steffan van de Poll <steffanvandepoll@gmail.com>
2019-06-28 13:44:22 +0200

    Add dynamic list for products in localStorage

--- great-plactic-race-app/src/app/product-list/product-list.component.ts
+++ great-plactic-race-app/src/app/product-list/product-list.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  itemList;
+  totalWeight;
+
+  constructor() {
+    this.itemList = JSON.parse(window.localStorage.getItem("itemList"));
+
+    if(!this.itemList || this.itemList.length == 0){
+      this.initListIfEmpty();
+    }
+    let weight = 0;
+    [].forEach.call(this.itemList, function(item) {
+        weight += item.weight;
+    });
+
+    this.totalWeight = weight;
+  }
+
+  initListIfEmpty(){
+    let list = [];
+
+    let item1 = {date:"06-28-2019", name:"Coca Cola bottle 1,5L", weight:432};
+    let item2 = {date:"06-28-2019", name:"Coca Cola bottle 1,5L", weight:345};
+    let item3 = {date:"06-28-2019", name:"Coca Cola bottle 1,5L", weight:185};
+
+    window.localStorage.setItem("itemList", JSON.stringify([item1, item2, item3]));
+  }
 
   ngOnInit() {
   }
